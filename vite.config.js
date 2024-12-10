3e1914077f929c7934117ab8ecd90b0fd37ba147
hatem sobhy okasha okasha <omar.hatem.okasha@gmail.com>
2024-12-10 15:32:44 +0200

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  
   base: "/the-little-teacher/", // This ensures correct base path for GitHub Pages
 });
